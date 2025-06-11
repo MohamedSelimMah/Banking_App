be69ad611248ac6a7c479145d70fe83d07addfc5
MohamedSelimMah <selimmahjoub43@gmail.com>
2025-06-11 14:31:39 +0200

    file encryptes with attributes and methids

---
+++ EncryptionUtil.java
@@ -0,0 +1,17 @@
+public class EncryptionUtil{
+	private String secretKey;
+
+	public EncryptionUtil(String secretKey){
+		this.secretKey=secretKey;
+	}
+
+	public String encrypt(String data){
+		//TODO: Returns encrypted Strings
+		return null;
+	}
+
+	public String decrypt(String encryptedData){
+		//TODO: Returns decrypted Strings
+		return null;
+	}
+}
