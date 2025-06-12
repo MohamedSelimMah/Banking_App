c8adb8163894f81410dfc1f34eb1a5fef902d33e
MohamedSelimMah <selimmahjoub43@gmail.com>
2025-06-13 00:01:42 +0200

    App.java is done just need to do the methods func

---
+++ App.java
@@ -0,0 +1,31 @@
+public class App{
+	private BankClass bank;
+
+	public App(BankClass bank){
+		this.bank=bank;
+	}
+
+	public void displayMenu(){
+		//TODO: Displays Main Menu no return value
+	}
+
+	public void createAccount(){
+		//TODO: HNAdels user input for account creation
+	}
+
+	public void login(){
+		//TODO: Authenticates user and directs to account creation
+	}
+
+	public void handleLoggedInUser(AccountClass account){
+		//TODO: Manages logged-in user operations
+	}
+
+	public void listAcc(){
+		//TODO: Shows admin-only account list
+	}
+
+	public void exitApp(){
+		//TODO: Ends the program
+	}
+}
