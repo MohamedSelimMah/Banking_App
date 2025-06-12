ebe64afeed5c2d445394273a4cf0f8cedc792d8f
MohamedSelimMah <selimmahjoub43@gmail.com>
2025-06-12 23:34:30 +0100

    EncryptionUtil done with all functions

--- EncryptionUtil.java
+++ EncryptionUtil.java
@@ -1,17 +1,37 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class EncryptionUtil{
-	private String secretKey;
+	
+	private final SecretKey secretKey;
 
-	public EncryptionUtil(String secretKey){
-		this.secretKey=secretKey;
+	public EncryptionUtil() throws Exception{
+		this.secretKey= generateKey();
 	}
 
-	public String encrypt(String data){
-		//TODO: Returns encrypted Strings
-		return null;
+	public SecretKey generateKey() throws Exception{
+		KeyGenerator KeyG=  KeyGenerator.getInstance("AES");
+		KeyG.init(128);
+		return KeyG.generateKey();
+	}
+	public String encrypt(String data) throws Exception {
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+		byte[] encryptedData = cipher.doFinal(data.getBytes());
+		return Base64.getEncoder().encodeToString(encryptedData);	
 	}
 
-	public String decrypt(String encryptedData){
-		//TODO: Returns decrypted Strings
-		return null;
+	public String decrypt(String encryptedData)throws Exception{
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.DECRYPT_MODE, secretKey);
+		byte[] decryptedData = Base64.getDecoder().decode(encryptedData);
+		return new String(cipher.doFinal(decryptedData));
+	}
+
+	public String getSecretKey(){
+		return Base64.getEncoder().encodeToString(secretKey.getEncoded());
 	}
 }
